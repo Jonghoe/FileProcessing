623d9d971de849534f0e48f8c536173231d6098e
DESKTOP-DQ7BBK3\rnwhd <rnwhdghl12@naver.com>
2017-05-15 20:51:19 +0900

    Hash table의 Bucket class 정의

--- second/Index_Hashing/Bucket.cpp
+++ second/Index_Hashing/Bucket.cpp
@@ -1,23 +1,46 @@
 #include"Bucket.h"
 #include<cassert>
 int Bucket::nextBlkNum = 0;
-Bucket::Bucket():capacity(0), size(BLOCK_SIZE / sizeof(Student)),blkNum(Bucket::blkNum++)
+Bucket::Bucket() :capacity(0), size(4), blkNum(nextBlkNum++), level(1)
 {
 	records = new Student[size];
 }
+
+Bucket::Bucket(int l) : capacity(0), size(4), blkNum(nextBlkNum++), level(l)
+{
+	records = new Student[size];
+}
+
 Student Bucket::erase(int position)
 {
-	assert(capacity <= position);
-	Student temp = records[capacity - 1];
-	records[capacity - 1] = records[position];
-	records[position] = temp;
+	assert(position < capacity);
+	Student temp = records[position];
+	records[position] = records[capacity - 1];
+	records[capacity - 1] = temp;
 	--capacity;
+	return temp;
+}	
+
+int Bucket::getCapacity()const
+{
+	return capacity;
+}
+
+Student& Bucket::operator[](int idx)const
+{
+	return records[idx];
 }
 
 int Bucket::insert(Student & data)
 {
-	if (capacity == size)
-		return -1;
+	if (capacity == size){
+		++level;
+		return -1;		
+	}
 	records[capacity++] = data;
 	return 0;
+}
+
+int Bucket::getLevel()const{
+	return level;
 }
--- second/Index_Hashing/Bucket.h
+++ second/Index_Hashing/Bucket.h
@@ -5,16 +5,21 @@ class Bucket {
 protected:
 	static int nextBlkNum;
 	const int size;
+	int level;
 	int capacity;
 	Student* records;
 	int blkNum;
 public:
+	Bucket(int l);
 	Bucket();
-
+	int getBlkNum() {
+		return blkNum;
+	}
 	//position의 record를 삭제 및 반환
 	Student erase(int position);
-
+	int getLevel()const;
 	// data를 array 마지막에 삽입 size를 넘어가면 -1 반환
 	int insert(Student& data);
-	
+	int getCapacity()const;
+	Student& operator[](int idx)const;
 };
