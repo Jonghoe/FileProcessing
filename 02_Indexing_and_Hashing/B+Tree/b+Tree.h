ccefb2f4ef0501a3b83f312487040a35973fd622
Joowon <joowon7508@gmail.com>
2017-05-22 18:12:01 +0900

    made BPlusTree class

--- 02_Indexing_and_Hashing/B+Tree/b+Tree.h
+++ 02_Indexing_and_Hashing/B+Tree/b+Tree.h
@@ -1,8 +1,12 @@
+#ifndef BPLUSTREE_H
+#define BPLUSTREE_H
 
 class BPlusTree{
  protected:
+  InternalNode* rootNode;
  public:
-  
+  BPlusTree();
+  bool insert(unsigned int studentID, unsigned int blockNum);
 };
 
 class InternalNode {
@@ -14,3 +18,5 @@ class InternalNode {
 
 class TerminalNode {
 };
+
+#endif
