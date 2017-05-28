49c737bfdffbe99e5b4e5dc61f0778df9bd6878f
Joowon <joowon7508@gmail.com>
2017-05-28 20:19:25 +0900

    prob in insertion

--- 02_Indexing_and_Hashing/B+Tree/b+Tree.cpp
+++ 02_Indexing_and_Hashing/B+Tree/b+Tree.cpp
@@ -1,12 +1,22 @@
 #include "b+Tree.h"
 
-BPlusTree::BPlusTree() {}
-bool BPlusTree::storeTree() {
-}
-bool BPlusTree::readTree() {
-}
+BPlusTree::BPlusTree() : rootNode(new TerminalNode()) {}
+bool BPlusTree::storeTree() {} // tngud's part (store the structure in a file)
+bool BPlusTree::readTree() {}  // tngus's part (read the structure from a file)
   
 bool BPlusTree::insert(float score, int blockNum) {
+  Node* lookingAt = rootNode;
+
+  while(1) {
+    if (lookingAt.ifTermianl()) { // found terminal
+      if (!lookingAt.insert(score, blockNum)) { // should do sth if no more space
+	
+      }
+    }
+    else { // try finding terminal
+      lookingAt = lookingAt.search(score); // this is the first node match
+    }
+  }
 }
 int  BPlusTree::exactMatchSearch(float score) {
 }
@@ -15,10 +25,27 @@ int* BPlusTree::rangeSearch(float score) {
 
 InternalNode::InternalNode() : branchSize(512), scoreSize(511) {}
 Node* InternalNode::search(float score) {
+  if (score < scoreDeli[0])
+    return branchs[0];
+  else {
+    // this is for checking 1..(size-1)
+    int checkSize = storedRecordNumber;
+    if (checkSize == scoreSize)
+      checkSize--;
+    
+    for (int i = 0; i < checkSize; i++) {
+      if (scoreDeli[i] <= score && score < scoreDeli[i+1])
+	return branchs[i];
+    }
+    if (storedRecordNumber == scoreSize && score < scoreDeli[scoreSize-1])
+      return branchs[branchSize-1]
+      
+  }
 }
 
-TerminalNode::TerminalNode() : size(511){}
-  
+TerminalNode::TerminalNode() : size(511) {}
+bool TerminalNode::insert(float score, int blockNum) {
+}  
 int TerminalNode::getBlockNumber(float score) {
 }
 int TerminalNode::getRangeBlockNumber(float score) {
--- 02_Indexing_and_Hashing/B+Tree/b+Tree.h
+++ 02_Indexing_and_Hashing/B+Tree/b+Tree.h
@@ -6,6 +6,7 @@ class Node {};
 class InternalNode;
 class TerminalNode;
 
+
 class BPlusTree{
 protected:
   Node* rootNode;
@@ -20,18 +21,21 @@ public:
   int* rangeSearch(float score);
 };
 
+
 class InternalNode: public Node {
   friend class BPlusTree;
   
 private:
-  const int branchSize;
-  const int scoreSize;
+  const int branchSize; //512
+  const int scoreSize;  //511
   int storedRecordNumber;
 
   Node* branchs[512];
   float scoreDeli[511];
+  InternalNode* parent;
 protected:
   InternalNode();
+  bool  ifTerminal() {return false;}
   
   Node* search(float score);
 };
@@ -46,11 +50,14 @@ private:
   int storedRecordNumber;
 
   float scores[511];
-  int   blockNum[511];
+  int   blockNum[511]; // this should be changed into pointer
+  InternalNode* parent;
   TerminalNode* nextTerminalNode;
 protected:
   TerminalNode();
-  
+  bool  ifTerminal() {return true;}
+
+  bool insert(float score, int blockNum);
   int getBlockNumber(float score);
   int getRangeBlockNumber(float score);
 };
