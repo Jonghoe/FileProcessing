d95e8f946bdb8bbc21bc88f0c5d5b2ce55c8bc2a
DESKTOP-DQ7BBK3\rnwhd <rnwhdghl12@naver.com>
2017-06-01 22:34:33 +0900

    main

--- 02_Indexing_and_Hashing/Index_Hashing/main.cpp
+++ 02_Indexing_and_Hashing/Index_Hashing/main.cpp
@@ -3,20 +3,55 @@
 #include<bitset>
 #include"HashTable.h"
 using namespace std;
-
+void printMenu()
+{
+	cout << "=================================================================================================================" << endl;
+	for(int i=0;i<15;++i)
+		cout << "#\t\t\t\t\t\t\t\t\t\t\t\t\t\t#\n";
+	cout << "=================================================================================================================" << endl;
+}
+void printExit()
+{
+	cout << "=================================================================================================================" << endl;
+	for (int i = 0; i<15; ++i)
+		cout << "#\t\t\t\t\t\t\t\t\t\t\t\t\t\t#\n";
+	cout << "=================================================================================================================" << endl;
+}
+void body()
+{
+	const int MAIN = 0;
+	const int EXIT = 1;
+	const int INPUT = 2;
+	int status = MAIN;
+	while (status == EXIT) {
+		switch (status)
+		{
+		case MAIN:
+			printMenu();
+			break;
+		case EXIT:
+			printExit();
+		default:
+			break;
+		}
+	}
+}
 int main()
 {
-	
-	ifstream ifs("sampleData.csv",ios::in);
+
+	vector<int> a(3,3);
+	a[1]=1;
+	a[2]=2;
+	int* pt=&a[0];
+	cout << pt[0]<<pt[1]<<pt[2]<<endl;
+	printMenu();
+	/*ifstream ifs("sampleData.csv",ios::in);
 	string data;
 	char buffer[1024];
 	HashTable tlb;
 	vector<Student> students;
 	ifs.getline(buffer, 1024);
 	int num = atoi(buffer);
-
-	const vector<int>& tb = tlb.getTable();
-	
 	for(int i=0;i<num;++i){
 		ifs.getline(buffer, 1024);
 		char* buff = buffer;
@@ -28,7 +63,6 @@ int main()
 		next = strlen(data);
 		int j = MyStrCpy(stu.name, data);
 		
-		cout << stu.name;
 		buff = buff + next+1;
 
 		data = strtok(buff, ",");
@@ -47,9 +81,6 @@ int main()
 		students.push_back(stu);
 		tlb.insert(students[i]);
 	}
+	ifs.close();*/
 	
-	ifs.close();
-	cout << "==================================================" << endl;
-	tlb.printBuckets();
-	cout << "##################################################" << endl;
 }
