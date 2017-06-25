795c0b6d0df3816b5a214ec7d903771d2eb02ef0
Joowon <joowon7508@gmail.com>
2017-06-25 18:53:27 +0900

    made float -> int

---
+++ 02_Indexing_and_Hashing/B+Tree/b+TreeIO2.cpp
@@ -0,0 +1,139 @@
+#ifndef BPLUSTREEIO_CPP
+#define BPLUSTREEIO_CPP
+
+#include "b+Tree.h"
+#include <iostream>
+//#include <fstream>
+#include <stdio.h>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+void indentPrint(int indent) {
+  using namespace std;
+  for (int i = 0; i < indent; i++)
+    cout << "\t";
+}
+
+// these are for print test (block number values are pointer address)
+void BPlusTree::print() {rootNode->print(0);}
+void InternalNode::print(int indent) {
+  indentPrint(indent);
+  cout << "<<Internal Node>> : " << allocatedBlockNumber << endl;
+  indentPrint(indent);
+  cout << "num of records : " << storedRecordNumber << endl;
+  for (int i = 0; i < storedRecordNumber-1; i++) {
+    indentPrint(indent);
+    cout << "branchs[" << i << "] : " << branchs[i]->allocatedBlockNumber << "\t";
+    cout << "scoreDeli[" << i << "] : " << scoreDeli[i] << endl;
+  }
+  indentPrint(indent);
+  cout << "branchs[" << storedRecordNumber-1 << "] : " << branchs[storedRecordNumber-1]->allocatedBlockNumber << endl << endl;
+
+  for (int i = 0; i < storedRecordNumber; i++) {
+    indentPrint(indent);
+    cout << "branchs[" << i << "]'s child" << endl;
+    branchs[i]->print(indent+1);
+  }
+}
+void TerminalNode::print(int indent) {
+  indentPrint(indent);
+  cout << "<<Terminal Node>> : " << allocatedBlockNumber << endl;
+  indentPrint(indent);
+  cout << "num of records : " << storedRecordNumber << endl;
+  for (int i = 0; i < storedRecordNumber; i++) {
+    indentPrint(indent);
+    cout << "scores[" << i << "] : " << scores[i] << "\t";
+    cout << "studID[" << i << "] : " << studID[i] << endl;
+  }
+  cout << endl;
+  if (nextTerminalNode != NULL)
+    cout << "next BlockNumber: " << nextTerminalNode->allocatedBlockNumber << endl;
+  else
+    cout << "next BlockNumber: None"  << endl;
+}
+
+
+/*
+// actually print blockNum
+void BPlusTree::printWithBlockNum(const HashTable& hashTable) {
+  rootNode->printWithBlockNum(0, hashTable);
+}
+
+void InternalNode::printWithBlockNum(int indent, const HashTable& hashTable) {
+  indentPrint(indent);
+  cout << "<<Internal Node>> : " << allocatedBlockNumber << endl;
+  indentPrint(indent);
+  cout << "num of records : " << storedRecordNumber << endl;
+  for (int i = 0; i < storedRecordNumber-1; i++) {
+    indentPrint(indent);
+    cout << "branchs[" << i << "] : " << branchs[i]->allocatedBlockNumber << "\t";
+    cout << "scoreDeli[" << i << "] : " << scoreDeli[i] << endl;
+  }
+  indentPrint(indent);
+  cout << "branchs[" << storedRecordNumber-1 << "] : " << branchs[storedRecordNumber-1]->allocatedBlockNumber << endl << endl;
+
+  for (int i = 0; i < storedRecordNumber; i++) {
+    indentPrint(indent);
+    cout << "branchs[" << i << "]'s child" << endl;
+    branchs[i]->printWithBlockNum(indent+1, hashTable);
+  }
+}
+
+void TerminalNode::printWithBlockNum(int indent, const HashTable& hashTable) {
+  indentPrint(indent);
+  cout << "<<Terminal Node>> : " << allocatedBlockNumber << endl;
+  indentPrint(indent);
+  cout << "num of records : " << storedRecordNumber << endl;
+  for (int i = 0; i < storedRecordNumber; i++) {
+    indentPrint(indent);
+    cout << "scores[" << i << "] : " << scores[i] << "\t";
+    cout << "blocks[" << i << "] : " << studID[i] << endl;
+    //cout << "blocks[" << i << "] : " << hashTable.getBlkNum(blockNum[i]) << endl;
+  }
+  cout << endl;
+}
+*/
+
+int readStudent(BPlusTree* tree) {
+  int studNum;
+  
+  char nameTemp[21];
+  int numTemp, profNumTemp;
+  //  int scoreTemp, scoreTemp2, length;
+  char scoreTemp[10]; int scoreCal, i;
+  //  ifstream readStud;
+  //  readStud.open("SampleData/student_data.csv");
+  FILE *stFP;
+  
+  stFP = fopen("./SampleData/student_data.csv", "r");
+  if(!stFP){
+    printf("hello");
+  }
+  fscanf(stFP, "%d\n", &studNum);
+  printf("%d\n", studNum);
+
+  //  studNum = 10;
+
+  //  readStud >> studNum;
+  while(studNum--) {
+    fscanf(stFP, "%[^,],%d,%[^,],%d\n",  nameTemp, &numTemp, scoreTemp, &profNumTemp);
+    scoreCal = scoreTemp[0] - '0';
+    i = 1;
+    for (; scoreTemp[i] != '\0'; i++) {
+      if (scoreTemp[i] == '.')
+	continue;
+      scoreCal = (scoreCal * 10) + (scoreTemp[i] - '0');
+    }
+    if (i == 1)
+      i = 2;
+    for (; i < 10; i++)
+      scoreCal *= 10;
+    
+    //printf("%s\t%d\t%d\t%d\n", nameTemp, numTemp, scoreCal, profNumTemp);
+    tree->insert(scoreCal, numTemp);    
+  }
+};
+
+#endif
--- 02_Indexing_and_Hashing/B+Tree/b+TreeSearch.cpp
+++ 02_Indexing_and_Hashing/B+Tree/b+TreeSearch.cpp
@@ -4,12 +4,12 @@
 #define BPLUSTREESEARCH_CPP
 
 
-Node* BPlusTree::searchFirstMatch(float scoreLowerBound) {
+Node* BPlusTree::searchFirstMatch(int scoreLowerBound) {
   return rootNode->searchFirstMatch(scoreLowerBound);
 }
 
 // search for first corresponding score low bound
-Node* InternalNode::searchFirstMatch(float scoreLowerBound) {
+Node* InternalNode::searchFirstMatch(int scoreLowerBound) {
   // 1. checking the first index which is branch[0]
   if (scoreLowerBound < scoreDeli[0])
     return branchs[0]->searchFirstMatch(scoreLowerBound);
@@ -21,26 +21,26 @@ Node* InternalNode::searchFirstMatch(float scoreLowerBound) {
       if (scoreDeli[i-1] <= scoreLowerBound && scoreLowerBound < scoreDeli[i])
 	return branchs[i]->searchFirstMatch(scoreLowerBound);
     }
-    // 3. checking the last index
-    if (scoreDeli[checkSize-1] <= scoreLowerBound)
-      return branchs[checkSize]->searchFirstMatch(scoreLowerBound);
+    // 3. checking the last index (always match if no match above)
+    //if (scoreDeli[checkSize-1] <= scoreLowerBound)
+    return branchs[checkSize]->searchFirstMatch(scoreLowerBound);
   }
 }
 
-Node* TerminalNode::searchFirstMatch(float scoreLowerBound) {
+Node* TerminalNode::searchFirstMatch(int scoreLowerBound) {
   return this;
 }
 
 // function to help Terminal::search()
 // count how may records correspond to search
-int TerminalNode::cntTillUpper(float scoreUpperBound) {
+int TerminalNode::cntTillUpper(int scoreUpperBound) {
   // if upperbound is bigger than max
   if (maxVal() < scoreUpperBound) {
     int nextNums = 0;
-    if (nextTerminalNode != NULL)
+    if (nextTerminalNode != NULL) // get nextNums
       nextNums = nextTerminalNode->cntTillUpper(scoreUpperBound);
-    else
-      return storedRecordNumber + nextNums;
+    
+    return storedRecordNumber + nextNums;
   }
   // other cases
   else {
@@ -74,7 +74,7 @@ bool TerminalNode::cpyMatchRecords(int* blockNums, int startIndex, int cpyLeft) 
     return false;
 }
 
-int* TerminalNode::search(float scoreLowerBound, float scoreUpperBound) {
+int* TerminalNode::search(int scoreLowerBound, int scoreUpperBound) {
   // if first match node is not in this node but next one
   if (maxVal() < scoreLowerBound) {
     if (nextTerminalNode != NULL)
